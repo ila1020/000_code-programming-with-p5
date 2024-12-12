c6d85b3e7da9b66c61b03fa31cc1c5331490a39b
ila1020 <ilaria.magni@mail.polimi.it>
2024-12-12 15:57:47 +0100

    font2

--- load.js
+++ load.js
@@ -1,7 +1,22 @@
+let font;
+
+
+function preload(){
+  font = loadFont('/desktop/Ribes-Regular.otf');
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  fill('black');
+  textFont(font);
+  textSize(36);
+  text('p5*js', 10, 50);
+
+  describe('LOADING');
 }
 
 function draw() {
-  background(220);
+  background(255,20,147);
+  
 }
